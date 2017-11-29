9749e6e34ab3309045e89348a3c5b88b1eddb62f
Andre Velez <drdre2266@gmail.com>
2017-11-29 11:41:08 -0600

    -Added regression models.  Cleaned up code formatting.  Added Data Viz section.

--- Capstone_tidy.R
+++ Capstone_tidy.R
@@ -1,12 +1,21 @@
-# Load needed packages #
+### -------------------------------------------- ###
+###               Data Extraction                ###
+### -------------------------------------------- ###
+
+# Load needed packages
 library(tidyr)
 library(dplyr)
 library(readr)
 library(readxl)
 library(magrittr)
 library(stringi)
+library(GGally)
+library(lme4)
+library(ggplot2)
+library(caret)
+library(effects)
 
-# Import raw data as objects, remove rows w/o observations #
+# Import raw data as objects, remove rows w/o observations
 crime_13_raw <- read_excel("Raw Data/2013 Crime Data/Table_8_Offenses_Known_to_Law_Enforcement_by_State_by_City_2013.xls", skip = 2)
 crime_14_raw <- read_excel("Raw Data/2014 Crime Data/Table_8_Offenses_Known_to_Law_Enforcement_by_State_by_City_2014.xls", skip = 2)
 crime_15_raw <- read_excel("Raw Data/2015 Crime Data/Table_8_Offenses_Known_to_Law_Enforcement_by_State_by_City_2015.xls", skip = 2)
@@ -15,26 +24,27 @@ unemployment_raw <- read_csv("Raw Data/Unemployment Data/unemployment_raw.csv")
 population_raw <- read_csv("Raw Data/City Population/sub-est2016_all.csv")
 city_county_raw <- read_delim("Raw Data/City by County/us_cities_states_counties.csv", delim = "|")
 
+## --------------------------------------------
+## Crime Data
+## --------------------------------------------
 
-### Crime Data ###
-
-# Rename header and remove nonsense rows # 
+# Rename header and remove nonsense rows 
 crime_13_detailed <- crime_13_raw %>%
   select(., 1:14) %>%
   set_colnames(.[1,]) %>% 
   slice(2:9293) %>%
   
-# Assign the correct data type #
+  # Assign the correct data type
   mutate_at(., 1:2, funs(as.character)) %>%
   mutate_at(., 3:14, funs(as.integer)) %>% 
-
-# Replace and Fill missing State values #  
+  
+  # Replace and Fill missing State values  
   fill(State, .direction = c("down")) %>% 
   
-# Sum crime data into one variable #
+  # Sum crime data into one variable
   mutate(Total_crime_13 = rowSums(.[4:14], na.rm = T, dims = 1))
 
-# Tidy format, clean up names #
+# Tidy format, clean up names
 crime_13_tidy <- crime_13_detailed %>% 
   select(., 1:2, 4, 15) %>% 
   mutate(City = gsub("\\d.*", "", .$City)) %>% 
@@ -42,9 +52,11 @@ crime_13_tidy <- crime_13_detailed %>%
 
 names(crime_13_tidy)[3] <- c("Violent_crime_13")
 
-### Iterate for 2014 and 2015 crime data ###
+## --------------------------------------------
+## Iterate for 2014 and 2015 crime data
+## --------------------------------------------
 
-# Tidy 2014 Crime Data #
+# Tidy 2014 Crime Data 
 crime_14_detailed <- crime_14_raw %>%
   select(., 1:14) %>%
   set_colnames(.[1,]) %>% 
@@ -61,7 +73,7 @@ crime_14_tidy <- crime_14_detailed %>%
 
 names(crime_14_tidy)[3] <- c("Violent_crime_14")
 
-# Tidy 2015 Crime Data #
+# Tidy 2015 Crime Data 
 crime_15_detailed <- crime_15_raw %>%
   select(., 1:14) %>%
   set_colnames(.[1,]) %>% 
@@ -78,10 +90,11 @@ crime_15_tidy <- crime_15_detailed %>%
 
 names(crime_15_tidy)[3] <- c("Violent_crime_15")
 
+## --------------------------------------------
+## City and County Data
+## --------------------------------------------
 
-### City and County Data ###
-
-# Remove unnecessary variables / observations, and place into tidy format #
+# Remove unnecessary variables / observations, and place into tidy format 
 city_county_tidy <- city_county_raw %>% 
   select(., 1, 3:4) %>% 
   rename(., State = "State full") %>% 
@@ -89,14 +102,15 @@ city_county_tidy <- city_county_raw %>%
   mutate_at(., 1:3, funs(as.character)) %>% 
   mutate(County = stri_trans_totitle(County))
 
+## --------------------------------------------
+## Unemployment Data
+## --------------------------------------------
 
-### Unemployment Data ###
-
-# Remove county names #
+# Remove county names
 unemployment_tidy <- unemployment_raw %>% 
   mutate(County = gsub("\\sCounty.*", "", .$County)) %>% 
-
-# Spread data to determine annual rate, tidy up # 
+  
+  # Spread data to determine annual rate, tidy up 
   spread(., Month, Rate) %>% 
   mutate(Annual_rate = rowMeans(.[4:15], na.rm = T, dims = 1)) %>% 
   filter(., Year > 2012 & Year < 2016) %>% 
@@ -104,14 +118,15 @@ unemployment_tidy <- unemployment_raw %>%
   spread(., Year, Annual_rate) %>% 
   rename(., Unemp_rate_13 = "2013", Unemp_rate_14 = "2014", Unemp_rate_15 = "2015")
 
+## --------------------------------------------
+## College Score Data
+## --------------------------------------------
 
-### College Score Data ###
-
-# Remove unnecessary variables #
+# Remove unnecessary variables
 college_score_tidy <- college_score_raw %>% 
   select(., 4:5, 7, 10:12, 85, 97:99, 110:111, 115, 117:119, 123) %>% 
-
-# Rename varibles for readability #
+  
+  # Rename varibles for readability
   rename(., Uni = INSTNM, 
          City = CITY, 
          State = STATE, 
@@ -132,15 +147,16 @@ college_score_tidy <- college_score_raw %>%
   
   mutate_at(., 9:17, funs(as.numeric))
 
+## --------------------------------------------
+## Population Data
+## --------------------------------------------
 
-### Population Data ###
-
-# Remove unnecessary variables #
+# Remove unnecessary variables
 population_tidy <- population_raw %>% 
   select(., 9:10, 16:18) %>% 
   mutate_at(., 1:2, funs(as.character)) %>% 
-
-# Tidy variable and observation names #
+  
+  # Tidy variable and observation names
   rename(., City = NAME, 
          State = STNAME, 
          pop_13 = POPESTIMATE2013,
@@ -154,8 +170,9 @@ population_tidy <- population_raw %>%
   mutate(City = gsub(".*\\(pt.)", "", .$City)) %>% 
   distinct(.)
 
-
-### Merged Tidy Dataframe ###
+## --------------------------------------------
+## Merged Tidy Dataframe
+## --------------------------------------------
 
 crime_1 <- left_join(crime_13_tidy, crime_14_tidy, by = c("State", "City"))
 
@@ -167,27 +184,26 @@ crime_county <- left_join(crime_pop, city_county_tidy, by = c("State", "City"))
 
 risk_tidy <- left_join(crime_county, unemployment_tidy, by = c("State", "County"))
 
-# Violent crime rate(VCR) per capita of cities > or = to 100,000 population size #
+# Violent crime rate(VCR) per capita of cities > or = to 100,000 population size
 risk_VCR <- risk_tidy %>% 
   mutate(VCR_13 = ifelse(.$pop_13 < 100000, NA, (.$Violent_crime_13 / .$pop_13 * 100))) %>% 
   mutate(VCR_14 = ifelse(.$pop_14 < 100000, NA, (.$Violent_crime_14 / .$pop_14 * 100))) %>% 
   mutate(VCR_15 = ifelse(.$pop_15 < 100000, NA, (.$Violent_crime_15 / .$pop_15 * 100))) %>% 
   drop_na(.)
 
-# Separate expanded df to incorporate college data #
+# Separate expanded df to incorporate college data
 risk_VCR_final <- left_join(risk_VCR, college_score_tidy, by = c("State", "City"))
 
+### -------------------------------------------- ###
+###         Exploratory Data Analysis            ###
+### -------------------------------------------- ###
 
-### Data Visualization ###
-
-library(ggplot2)
-
-# Find the mean of VCR and Unemployment #
+# Find the mean of VCR and Unemployment
 risk_VCR_avg <- risk_VCR %>%
   mutate(VCR_avg = rowMeans(.[16:18], na.rm = T, dims = 1)) %>% 
   mutate(Unemp_rate_avg = rowMeans(.[13:15], na.rm = T, dims = 1))
 
-# Creat a new variable to facet plot between top and bottom 100 cities #
+# Creat a new variable to facet plot between top and bottom 100 cities
 top100_UR <- risk_VCR_avg %>%
   arrange(., desc(VCR_avg)) %>% 
   distinct(., VCR_avg, .keep_all = T) %>% 
@@ -200,22 +216,24 @@ bot100_UR <- risk_VCR_avg %>%
   top_n(., -100, VCR_avg) %>% 
   mutate(rank = "Least_Violent")
 
-# Plot the mean VCR against Uemployment rate for top and bottom 100 cities #
+# Plot the mean VCR against Uemployment rate for top and bottom 100 cities
 VCR_UR <- bind_rows(top100_UR, bot100_UR)
 
 VCR_UR_scat <- ggplot(VCR_UR, aes(x = Unemp_rate_avg, y = VCR_avg)) +
-                    geom_point(size = 2, shape = 1, alpha = 0.6) +
-                    geom_smooth(method = "lm", se = T) +
-                    facet_grid(. ~ rank)
+  geom_point(size = 2, shape = 1, alpha = 0.6) +
+  geom_smooth(method = "lm", se = T) +
+  facet_grid(. ~ rank)
 
-# Find avg grad rate by city #
+plot(VCR_UR_scat)
+
+# Find avg grad rate by city
 GR_by_city <- risk_VCR_final %>% 
   group_by(., City, State) %>% 
-  summarise(Grad_Rate_avg = mean(Grad_Rate, na.rm = T))
+  summarise(Grad_rate_avg = mean(Grad_Rate, na.rm = T))
 
 risk_VCR_avg_GR <- left_join(risk_VCR_avg, GR_by_city, by = c("State", "City"))
 
-# Plot mean VCR against college grad rate #
+# Plot mean VCR against college grad rate
 top100_GR <- risk_VCR_avg_GR %>% 
   arrange(., desc(VCR_avg)) %>% 
   distinct(., VCR_avg, .keep_all = T) %>% 
@@ -230,51 +248,54 @@ bot100_GR <- risk_VCR_avg_GR %>%
 
 VCR_GR <- bind_rows(top100_GR, bot100_GR)
 
-VCR_GR_scat <- ggplot(VCR_GR, aes(x = Grad_Rate_avg, y = VCR_avg)) +
-                  geom_point(size = 2, shape = 1, alpha = 0.6) +
-                  geom_smooth(method = "lm", se = T) +
-                  facet_grid(. ~ rank)
+VCR_GR_scat <- ggplot(VCR_GR, aes(x = Grad_rate_avg, y = VCR_avg)) +
+  geom_point(size = 2, shape = 1, alpha = 0.6) +
+  geom_smooth(method = "lm", se = T) +
+  facet_grid(. ~ rank)
 
-# Plot VCR, GR, and UR distributions for top and bot 100 VCR cities #
+plot(VCR_GR_scat)
 
+# Plot VCR, GR, and UR distributions for top and bot 100 VCR cities
 VCR_hist <- ggplot(VCR_GR, aes(x = VCR_avg)) +
-              geom_histogram(binwidth = 0.05) +
-              facet_grid(. ~ rank)
+  geom_histogram(binwidth = 0.04) +
+  facet_grid(. ~ rank)
 
-GR_hist <- ggplot(VCR_GR, aes(x = Grad_Rate_avg)) +
-              geom_histogram(binwidth = 0.05) +
-              facet_grid(. ~ rank)
+GR_hist <- ggplot(VCR_GR, aes(x = Grad_rate_avg)) +
+  geom_histogram(binwidth = 0.02) +
+  facet_grid(. ~ rank)
 
 UR_hist <- ggplot(VCR_UR, aes(x = Unemp_rate_avg)) +
-              geom_histogram(binwidth = 0.25) +
-              facet_grid(. ~ rank)
+  geom_histogram(binwidth = 0.25) +
+  facet_grid(. ~ rank)
 
-# Plot overlapping frequency for VCR, GR, and UR #
+plot(VCR_hist)
+plot(GR_hist)
+plot(UR_hist)
 
+# Plot overlapping frequency for VCR, GR, and UR
 VCR_freq <- ggplot(VCR_GR, aes(x = VCR_avg, col = rank)) +
-              geom_freqpoly(binwidth = 0.05)
+  geom_freqpoly(binwidth = 0.07)
 
-GR_freq <- ggplot(VCR_GR, aes(x = Grad_Rate_avg, col = rank)) +
-              geom_freqpoly(binwidth = 0.05)
+GR_freq <- ggplot(VCR_GR, aes(x = Grad_rate_avg, col = rank)) +
+  geom_freqpoly(binwidth = 0.1)
 
 UR_freq <- ggplot(VCR_UR, aes(x = Unemp_rate_avg, col = rank)) +
-              geom_freqpoly(binwidth = 0.25)
+  geom_freqpoly(binwidth = 0.7)
 
+plot(VCR_freq)
+plot(GR_freq)
+plot(UR_freq)
 
-# Load new packages necessary for total variable visualization #
-library(GGally)
-library(lme4)
-
-# Group city and state to find averages of college data #
+# Group city and state to find averages of college data
 risk_VCR_final_grouped <- risk_VCR_final %>% 
   mutate(Cost_total = rowMeans(.[25:26], na.rm = T, dims = 1)) %>% 
   group_by(., City, State) %>% 
-  summarise(., "Median_Income_avg" = mean(Median_Earnings_10years, na.rm = T),
-            "Median_Debt_avg" = mean(Median_Debt_Grads, na.rm = T),
-            "Retention_Rate_avg" = mean(Retention_Rate, na.rm = T),
+  summarise(., "Median_income_avg" = mean(Median_Earnings_10years, na.rm = T),
+            "Median_debt_avg" = mean(Median_Debt_Grads, na.rm = T),
+            "Retention_rate_avg" = mean(Retention_Rate, na.rm = T),
             "Cost_avg" = mean(Cost_total, na.rm = T))
 
-# Retain only averages of data from data exploration plots #
+# Retain only averages of data from data exploration plots
 matrix_1 <- VCR_GR %>% 
   select(., 1:2, 12, 19:22)
 
@@ -284,30 +305,146 @@ matrix_2 <- as_data_frame(matrix_2) %>%
   mutate_at(., 7, funs(as.factor))
 
 
-# Plot matrix data comparing all variables #
+# Plot matrix data comparing all variables
 comparison_plot <- ggpairs(matrix_2, 
-                             columns = c(4:6, 8:11),
-                             title = "100 Most Violent Cities vs 100 Least Violent Cities",
-                             upper = list(
-                               continuous = "cor",
-                               mapping = aes(color = rank, alpha = 0.6)
-                             ),
-                             lower = list(
-                               continuous = "smooth",
-                               mapping = aes(color = rank, alpha = 0.6)
-                             ),
-                             diag = list(
-                               continuous = "densityDiag",
-                               mapping = aes(color = rank, alpha = 0.6)
-                             )
+                           columns = c(4:6, 8:11),
+                           title = "100 Most Violent Cities vs 100 Least Violent Cities",
+                           upper = list(
+                             continuous = "cor",
+                             mapping = aes(color = rank, alpha = 0.6)
+                           ),
+                           lower = list(
+                             continuous = "smooth",
+                             mapping = aes(color = rank, alpha = 0.6)
+                           ),
+                           diag = list(
+                             continuous = "densityDiag",
+                             mapping = aes(color = rank, alpha = 0.6)
+                           )
 )
 
-### Models and Predictions ###
+print(comparison_plot)
 
-# Linear Regression Models for Predicting Violent Crime Rate #
+### -------------------------------------------- ###
+###           Models and Predictions             ###
+### -------------------------------------------- ###
 
-# Check significance and multicolinearity of independent variables to select features #
+# Separate by rank variable into 2 separate data frames to create 2 separate linear regressions for 
+# comparison purposes
+MV_df <- filter(matrix_2, rank == "Most_Violent")
 
-# Check performance parameters of prediction models from Training Set #
+LV_df <- filter(matrix_2, rank == "Least_Violent")
 
-# Evaluate accuracy of models on Testing Set # 
+## --------------------------------------------
+## Linear Regression Models for Most Violent cities by numeric predictors
+## --------------------------------------------
+
+# Regession using all predictors available
+model_MV_1 <- lm(VCR_avg ~ Unemp_rate_avg + Grad_rate_avg + Median_income_avg + Median_debt_avg 
+                 + Retention_rate_avg + Cost_avg,
+                 data = MV_df)
+
+print(summary(model_MV_1))
+
+# Remove least correlated predictor, the avgerage cost of colleges
+model_MV_2 <- lm(VCR_avg ~ Unemp_rate_avg + Grad_rate_avg + Median_income_avg + Median_debt_avg 
+                 + Retention_rate_avg,
+                 data = MV_df)
+
+print(summary(model_MV_2))
+
+# Remove median income due to high multicolinearty with median debt
+model_MV_3 <- lm(VCR_avg ~ Unemp_rate_avg + Grad_rate_avg + Median_debt_avg + Retention_rate_avg,
+                 data = MV_df)
+
+print(summary(model_MV_3))
+
+# Repalce median debt with median income and compare to model 3
+model_MV_4 <- lm(VCR_avg ~ Unemp_rate_avg + Grad_rate_avg + Median_income_avg + Retention_rate_avg,
+                 data = MV_df)
+
+print(summary(model_MV_4))
+
+# Debt is more significant, expand on model 3 by removing retention rate of college 
+# students due to multicolinearity with graduation rate 
+
+model_MV_5 <- lm(VCR_avg ~ Unemp_rate_avg + Grad_rate_avg + Median_debt_avg,
+                 data = MV_df)
+
+print(summary(model_MV_5))
+
+## --------------------------------------------
+## Linear Regression Models for Least Violent cities by numeric predictors
+## --------------------------------------------
+
+# Regression using all predictors available
+model_LV_1 <- lm(VCR_avg ~ Unemp_rate_avg + Grad_rate_avg + Median_income_avg + Median_debt_avg 
+                 + Retention_rate_avg + Cost_avg,
+                 data = LV_df)
+
+print(summary(model_LV_1))
+
+# Remove least signifanct predictor, grauation rate of college students
+model_LV_2 <- lm(VCR_avg ~ Unemp_rate_avg + Median_income_avg + Median_debt_avg 
+                 + Retention_rate_avg + Cost_avg,
+                 data = LV_df)
+
+print(summary(model_LV_2))
+
+# Remove next least significant predictor, unemployment rate
+model_LV_3 <- lm(VCR_avg ~ Median_income_avg + Median_debt_avg + Retention_rate_avg + Cost_avg,
+                 data = LV_df)
+
+print(summary(model_LV_3))
+
+# Remove median income due to high multicolinearity with median debt and less correlation to VCR
+model_LV_4 <- lm(VCR_avg ~ Median_debt_avg + Retention_rate_avg + Cost_avg,
+                 data = LV_df)
+
+print(summary(model_LV_4))
+
+# Calculate RMSE for comparison against k-fold cross validation results of final models 
+# (highest F statistic, lowest p-value)
+RMSE_model_MV_5 <- sqrt(sum((residuals(model_MV_5)/(1-hatvalues(model_MV_5)))^2)/length(model_MV_5$residuals))
+
+print(RMSE_model_MV_5)
+
+RMSE_model_LV_4 <- sqrt(sum((residuals(model_LV_4)/(1-hatvalues(model_LV_4)))^2)/length(model_LV_4$residuals))
+
+print(RMSE_model_LV_4)
+
+## --------------------------------------------
+## Cross Validation of Linear Models
+## --------------------------------------------
+
+# Train and test linear regressions via a repeated k-fold cross validation
+tcont <- trainControl(method = "repeatedcv", number = 10, repeats = 10)
+
+cvmodel_MV_5 <- train(VCR_avg ~ Unemp_rate_avg + Grad_rate_avg + Median_debt_avg,
+                      data = na.omit(MV_df),
+                      trControl = tcont,
+                      method = "lm")
+
+cvmodel_LV_4 <- train(VCR_avg ~ Median_debt_avg + Retention_rate_avg + Cost_avg,
+                      data = na.omit(LV_df),
+                      trControl = tcont,
+                      method = "lm")
+
+# Plot the importance of the variables used
+plot(varImp(cvmodel_MV_5))
+
+plot(varImp(cvmodel_LV_4))
+
+# Print test results for each model
+print(cvmodel_MV_5)
+
+print(cvmodel_LV_4)
+
+### -------------------------------------------- ###
+###               Data Visualization             ###
+### -------------------------------------------- ###
+
+plot(allEffects(model_MV_5))
+
+plot(allEffects(model_LV_4))
+
